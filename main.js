841df90e6ebaae41c0366a79133c53acf2a41aa2
paulkani <paulkani.s@golden-hippo.com>
2023-10-04 11:24:58 +0530

    swiggy

---
+++ main.js
@@ -0,0 +1,628 @@
+let foods = [
+    {
+        id: 1,
+        name: "Vairamalikai Takeaway",
+        cuisine: "North Indian , South Indian",
+        imgUrl: "images/1.webp",
+        rating: "4.2",
+        preparetion: 150,
+        amount: 580,
+        units: "Veg & nonveg",
+        delivery: 30,
+        categoroy: "Both",
+    },
+    {
+        id: 2,
+        name: "Hotel Anandhabhavan",
+        cuisine: "North Indian , South Indian",
+        imgUrl: "images/2.webp",
+        rating: "3.9",
+        preparetion: 60,
+        amount: 280,
+        units: "Pure veg",
+        delivery: 20,
+        categoroy: "Veg",
+    },
+    {
+        id: 3,
+        name: "Hote Nellai Saravana..",
+        cuisine: "North Indian , South Indian",
+        imgUrl: "images/3.webp",
+        rating: "4.3",
+        preparetion: 130,
+        amount: 450,
+        units: "Veg & nonveg",
+        delivery: 45,
+        categoroy: "Both",
+    },
+    {
+        id: 4,
+        name: "Hotel Arya bavan",
+        cuisine: "North Indian , South Indian",
+        imgUrl: "images/4.webp",
+        rating: "4.2",
+        preparetion: 50,
+        amount: 160,
+        units: "Pure veg",
+        delivery: 30,
+        categoroy: "Veg",
+    },
+    {
+        id: 5,
+        name: "Hote Banu Birndavan..",
+        cuisine: "North Indian , South Indian",
+        imgUrl: "images/5.webp",
+        rating: "4.1",
+        preparetion: 30,
+        amount: 90,
+        units: "Pure veg",
+        delivery: 20,
+        categoroy: "Veg",
+    },
+    {
+        id: 6,
+        name: "Hotel Aryaas Grand",
+        cuisine: "North Indian , South Indian",
+        imgUrl: "images/6.webp",
+        rating: "4.4",
+        preparetion: 120,
+        amount: 350,
+        units: "Non veg",
+        delivery: 45,
+        categoroy: "Nonveg",
+    },
+    {
+        id: 7,
+        name: "Sri Maduram Hotel",
+        cuisine: "North Indian , South Indian",
+        imgUrl: "images/7.webp",
+        rating: "4.4",
+        preparetion: 160,
+        amount: 430,
+        units: "Non veg",
+        delivery: 30,
+        categoroy: "Nonveg",
+    },
+    {
+        id: 8,
+        name: "Sri Janaki Ram...",
+        cuisine: "North Indian , South Indian",
+        imgUrl: "images/8.webp",
+        rating: "4.3",
+        preparetion: 140,
+        amount: 480,
+        units: "Veg & nonveg",
+        delivery: 20,
+        categoroy: "Both",
+    },
+    {
+        id: 9,
+        name: "HI-FI Non Veg rest..",
+        cuisine: "North Indian , South Indian",
+        imgUrl: "images/9.webp",
+        rating: "4.1",
+        preparetion: 100,
+        amount: 390,
+        units: "Non veg",
+        delivery: 45,
+        categoroy: "Nonveg",
+    },
+    {
+        id: 10,
+        name: "Hotel Afna Park ",
+        cuisine: "North Indian , South Indian",
+        imgUrl: "images/10.webp",
+        rating: "4.1",
+        preparetion: 80,
+        amount: 340,
+        units: "Veg & nonveg",
+        delivery: 30,
+        categoroy: "Both",
+    },
+    {
+        id: 11,
+        name: "Sri Visaha Bhavan",
+        cuisine: "North Indian , South Indian",
+        imgUrl: "images/11.webp",
+        rating: "3.9",
+        preparetion: 60,
+        amount: 90,
+        units: "Pure veg",
+        delivery: 20,
+        categoroy: "Veg",
+    },
+    {
+        id: 12,
+        name: "Hotel sangeetha",
+        cuisine: "North Indian , South Indian",
+        imgUrl: "images/12.webp",
+        rating: "4.5",
+        preparetion: 110,
+        amount: 360,
+        units: "Pure veg",
+        delivery: 45,
+        categoroy: "Veg",
+    },
+    {
+        id: 13,
+        name: "Thiru Unavu Valagam",
+        cuisine: "North Indian , South Indian",
+        imgUrl: "images/13.webp",
+        rating: "3.9",
+        preparetion: 55,
+        amount: 410,
+        units: "Pure veg",
+        delivery: 30,
+        categoroy: "Veg",
+    },
+    {
+        id: 14,
+        name: "Pillaivall Mess",
+        cuisine: "North Indian , South Indian",
+        imgUrl: "images/14.webp",
+        rating: "4.3",
+        preparetion: 60,
+        amount: 230,
+        units: "Pure veg",
+        delivery: 20,
+        categoroy: "Veg",
+    },
+    {
+        id: 15,
+        name: "Hotel Sri Madhuram",
+        cuisine: "North Indian , South Indian",
+        imgUrl: "images/15.webp",
+        rating: "3.7",
+        preparetion: 70,
+        amount: 270,
+        units: "Pure veg",
+        delivery: 45,
+        categoroy: "Veg",
+    },
+    {
+        id: 16,
+        name: "Hotel HNR Hotel",
+        cuisine: "North Indian , South Indian",
+        imgUrl: "images/16.webp",
+        rating: "3.9",
+        preparetion: 210,
+        amount: 610,
+        units: "Veg & nonveg",
+        delivery: 30,
+        categoroy: "Both",
+    },
+    {
+        id: 17,
+        name: "Family Restarent",
+        cuisine: "North Indian , South Indian",
+        imgUrl: "images/17.webp",
+        rating: "4.2",
+        preparetion: 90,
+        amount: 210,
+        units: "Pure veg",
+        delivery: 20,
+        categoroy: "Veg",
+    },
+    {
+        id: 18,
+        name: "Ramesh Sweets & ba..",
+        cuisine: "North Indian , South Indian",
+        imgUrl: "images/18.webp",
+        rating: "3.8",
+        preparetion: 60,
+        amount: 330,
+        units: "Pure veg",
+        delivery: 45,
+        categoroy: "Veg",
+    },
+    {
+        id: 19,
+        name: "Hotel Uma Sankar",
+        cuisine: "North Indian , South Indian",
+        imgUrl: "images/19.webp",
+        rating: "3.5",
+        preparetion: 130,
+        amount: 530,
+        units: "Non veg",
+        delivery: 30,
+        categoroy: "Nonveg",
+    },
+    {
+        id: 20,
+        name: "Hotel Thiru unavu",
+        cuisine: "North Indian , South Indian",
+        imgUrl: "images/20.webp",
+        rating: "3.5",
+        preparetion: 110,
+        amount: 400,
+        units: "Veg & nonveg",
+        delivery: 20,
+        categoroy: "Both",
+    },
+    {
+        id: 21,
+        name: "Hotel Sri Ram ",
+        cuisine: "North Indian , South Indian",
+        imgUrl: "images/21.webp",
+        rating: "3.4",
+        preparetion: 50,
+        amount: 110,
+        units: "Pure veg",
+        delivery: 45,
+        categoroy: "Veg",
+    },
+    {
+        id: 22,
+        name: "Bahavathi Restarent",
+        cuisine: "North Indian , South Indian",
+        imgUrl: "images/22.webp",
+        rating: "3.2",
+        preparetion: 60,
+        amount: 140,
+        units: "Pure veg",
+        delivery: 30,
+        categoroy: "Veg",
+    },
+    {
+        id: 23,
+        name: "Ramesh Bakery",
+        cuisine: "North Indian , South Indian",
+        imgUrl: "images/23.webp",
+        rating: "3.2",
+        preparetion: 60,
+        amount: 110,
+        units: "Veg & nonveg",
+        delivery: 20,
+        categoroy: "Both",
+    },
+    {
+        id: 24,
+        name: "Hotel Nellai Special",
+        cuisine: "North Indian , South Indian",
+        imgUrl: "images/24.webp",
+        rating: "4.3",
+        preparetion: 40,
+        amount: 130,
+        units: "Pure veg",
+        delivery: 45,
+        categoroy: "Veg",
+    },
+    {
+        id: 25,
+        name: "Hotel Venkatesh",
+        cuisine: "North Indian , South Indian",
+        imgUrl: "images/25.webp",
+        rating: "3.3",
+        preparetion: 180,
+        amount: 630,
+        units: "Non veg",
+        delivery: 30,
+        categoroy: "Nonveg",
+    },
+    {
+        id: 26,
+        name: "Arusuvai Restarent",
+        cuisine: "North Indian , South Indian",
+        imgUrl: "images/26.webp",
+        rating: "3.7",
+        preparetion: 130,
+        amount: 430,
+        units: "Veg & nonveg",
+        delivery: 20,
+        categoroy: "Both",
+
+    },
+    {
+        id: 27,
+        name: "Hotel Kaveri",
+        cuisine: "North Indian , South Indian",
+        imgUrl: "images/27.webp",
+        rating: "3.2",
+        preparetion: 230,
+        amount: 730,
+        units: "Non veg",
+        delivery: 45,
+        categoroy: "Nonveg",
+    },
+    {
+        id: 28,
+        name: "5 star Hotel",
+        cuisine: "North Indian , South Indian",
+        imgUrl: "images/28.webp",
+        rating: "3.4",
+        preparetion: 190,
+        amount: 660,
+        units: "Non veg",
+        delivery: 30,
+        categoroy: "Nonveg",
+    },
+    {
+        id: 29,
+        name: "Bhuvi Non veg Hotel",
+        cuisine: "North Indian , South Indian",
+        imgUrl: "images/29.webp",
+        rating: "4.7",
+        preparetion: 145,
+        amount: 330,
+        units: "Non veg",
+        delivery: 30,
+        categoroy: "Nonveg",
+    },
+    {
+        id: 30,
+        name: "Hotel Uthai",
+        cuisine: "North Indian , South Indian",
+        imgUrl: "images/30.webp",
+        rating: "3.4",
+        preparetion: 90,
+        amount: 270,
+        units: "Non veg",
+        delivery: 45,
+        categoroy: "Nonveg",
+    },
+    {
+        id: 31,
+        name: "Hotel Thavasi",
+        cuisine: "North Indian , South Indian",
+        imgUrl: "images/31.webp",
+        rating: "3.1",
+        preparetion: 45,
+        amount: 230,
+        units: "Veg & nonveg",
+        delivery: 30,
+        categoroy: "Both",
+    },
+    {
+        id: 32,
+        name: "Kiramathu Samayal",
+        cuisine: "North Indian , South Indian",
+        imgUrl: "images/32.webp",
+        rating: "4.1",
+        preparetion: 165,
+        amount: 680,
+        units: "Non veg",
+        delivery: 20,
+        categoroy: "Nonveg",
+    },
+
+
+]
+
+
+let section = document.getElementById("fliter-section")
+// console.log(section);
+
+let container = document.createElement("div")
+container.classList.add("fliter-container");
+section.appendChild(container)
+
+let row = document.createElement("div")
+row.classList.add("fliter-row");
+container.appendChild(row)
+
+itemRender(foods)
+
+function itemRender(inpObj) {
+    // console.log(inpObj);
+
+    inpObj.forEach(function (e) {
+
+        let col = document.createElement("div")
+        col.classList.add("fliter-col");
+        col.classList.add(e.categoroy);
+        row.appendChild(col)
+
+        let card = document.createElement("div")
+        card.classList.add("fliter-card");
+        col.appendChild(card)
+
+        let cardImg = document.createElement("div")
+        cardImg.classList.add("fliter-img");
+        card.appendChild(cardImg)
+
+        let img = document.createElement("img")
+        img.setAttribute("src", e.imgUrl)
+        cardImg.appendChild(img)
+
+        let cardCon = document.createElement("div")
+        cardCon.classList.add("fliter-content");
+        card.appendChild(cardCon)
+
+        let heading = document.createElement("h1")
+        heading.classList.add("fliter-heading");
+        cardCon.appendChild(heading)
+        heading.innerHTML = e.name
+
+        let flIcon = document.createElement("div")
+        flIcon.classList.add("fliter-icons");
+        cardCon.appendChild(flIcon)
+
+        let icon = document.createElement("i")
+        icon.classList.add("fa")
+        icon.classList.add("fa-star")
+        icon.setAttribute("aria-hidden", true)
+        flIcon.appendChild(icon)
+
+        let para = document.createElement("p")
+        para.classList.add("fliter-para");
+        flIcon.appendChild(para)
+        para.innerHTML = e.rating
+
+        let para1 = document.createElement("p")
+        para1.classList.add("fliter-para1");
+        cardCon.appendChild(para1)
+        para1.innerHTML = e.cuisine
+
+        let price = document.createElement("p")
+        price.classList.add("fliter-price");
+        cardCon.appendChild(price)
+        price.innerHTML = "Rs. " + e.preparetion
+
+        let units = document.createElement("p")
+        units.classList.add("fliter-units");
+        cardCon.appendChild(units)
+        units.innerHTML = e.units
+
+        let units1 = document.createElement("p")
+        units1.classList.add("fliter-units1");
+        cardCon.appendChild(units1)
+        units1.innerHTML = "Bucket - " + "Rs. " + e.amount
+
+        let units2 = document.createElement("p")
+        units2.classList.add("fliter-units2");
+        cardCon.appendChild(units2)
+        units2.innerHTML = "Delivery By " + e.delivery + "  minutes"
+
+
+
+
+
+        if (e.rating >= 4.4) {
+            icon.style.backgroundColor = "red"
+            icon.style.color = "white"
+        }
+        else if (e.rating >= 4 && e.rating < 4.4) {
+            icon.style.backgroundColor = "green"
+            icon.style.color = "white"
+        }
+        else if (e.rating < 4 && e.rating >= 3.5) {
+            icon.style.backgroundColor = "yellow"
+            icon.style.color = "white"
+        }
+        else {
+            icon.style.backgroundColor = "blue"
+            icon.style.color = "white"
+        }
+
+    })
+
+}
+
+function defaultSorter() {
+
+    // let colsRemove = document.querySelectorAll(".fliter-col");
+    // colsRemove.forEach(function (e) {
+    //     e.remove()
+    // })
+    removecolum()
+    itemRender(foods)
+}
+
+function sortRating() {
+    let sortRatingObj = foods.slice().sort(function (a, b) {
+        return b.rating - a.rating;
+    })
+
+    // let colsRemove = document.querySelectorAll(".fliter-col");
+    // colsRemove.forEach(function (e) {
+    //     e.remove()
+    // })
+    removecolum()
+    itemRender(sortRatingObj)
+}
+
+function highRate() {
+    let highToLow = foods.slice().sort(function (a, b) {
+        return b.preparetion - a.preparetion;
+    })
+
+    // let colsRemove = document.querySelectorAll(".fliter-col");
+    // colsRemove.forEach(function (e) {
+    //     e.remove()
+    // })
+    removecolum()
+    itemRender(highToLow)
+}
+
+
+function rating() {
+    let fastDelivery = foods.slice().sort(function (a, b) {
+        return a.delivery - b.delivery;
+    })
+
+    // let colsRemove = document.querySelectorAll(".fliter-col");
+    // colsRemove.forEach(function (e) {
+    //     e.remove()
+    // })
+    removecolum()
+    itemRender(fastDelivery)
+}
+
+function pureVeg() {
+    defaultSorter()
+    let colsRemove = document.querySelectorAll(".Both");
+    colsRemove.forEach(function (e) {
+        e.remove()
+    })
+    let colssRemove = document.querySelectorAll(".Nonveg");
+    colssRemove.forEach(function (e) {
+        e.remove()
+    })
+}
+
+function nonVeg() {
+    defaultSorter()
+    let colsRemove = document.querySelectorAll(".Both");
+    colsRemove.forEach(function (e) {
+        e.remove()
+    })
+    let colssRemove = document.querySelectorAll(".Veg");
+    colssRemove.forEach(function (e) {
+        e.remove()
+    })
+}
+
+function vegNonVeg() {
+    defaultSorter()
+    let colsRemove = document.querySelectorAll(".Nonveg");
+    colsRemove.forEach(function (e) {
+        e.remove()
+    })
+    let colssRemove = document.querySelectorAll(".Veg");
+    colssRemove.forEach(function (e) {
+        e.remove()
+    })
+}
+
+
+function moreThan() {
+    // let colsRemove = document.querySelectorAll(".fliter-col");
+    // colsRemove.forEach(function (e) {
+    //     e.remove()
+    // })
+    removecolum()
+    let motrThanArray = [];
+    foods.forEach(function (e) {
+        if (e.preparetion > 100) {
+            motrThanArray.push(e)
+        }
+    })
+    itemRender(motrThanArray)
+
+}
+
+function lessThan() {
+    // let colsRemove = document.querySelectorAll(".fliter-col");
+    // colsRemove.forEach(function (e) {
+    //     e.remove()
+    // })
+    removecolum()
+    let motrThanArray = [];
+    foods.forEach(function (e) {
+        if (e.preparetion < 100) {
+            motrThanArray.push(e)
+        }
+    })
+    itemRender(motrThanArray)
+
+}
+
+function removecolum() {
+    let colsRemove = document.querySelectorAll(".fliter-col");
+    colsRemove.forEach(function (e) {
+        e.remove()
+    })
+}
+
+
+
